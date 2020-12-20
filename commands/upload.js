a4000ff1e2ff84aab297ea87c048828e726512f9
jrk8047 <jrk8047@rit.edu>
2020-12-20 13:56:51 -0500

    comments

--- commands/upload.js
+++ commands/upload.js
@@ -5,12 +5,14 @@ let sound_dest = './upload-sounds/';
 let supported_image_types = ['jpg', 'png'];
 let supported_sound_types = ['mp3'];
 
+// downloads the file from the url to the destination under the provided name
 function download(url, filename, dest){
     request.get(url)
         .on('error', console.error)
         .pipe(fs.createWriteStream(dest + filename));
 }
 
+// checks if the filetype is in the list of supported filetypes, wait a minute why did i make this a function i can do this in like 1 line
 function isSupported(filename, supported_filetypes){
     split_name = filename.split('.');
 
@@ -32,6 +34,7 @@ module.exports = {
             message.channel.send('In order to upload a file, attach your file to a new message and type !upload [title]');
             return;
         }
+        // make sure user attached a file
         if (!message.attachments.first())
         {
             message.channel.send('You forgot to attach a file, dipshit.');
